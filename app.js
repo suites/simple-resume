b6a13194869c158dea0c7067ce0235859f7d568b
yoon.homme <woosiks.io@gmail.com>
2019-02-15 10:46:25 +0900

    Add env port

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const path = require('path');
-const yaml = require('js-yaml');
-const fs = require('fs');
-var Autolinker = require( 'autolinker' );
+const express = require("express");
+const path = require("path");
+const yaml = require("js-yaml");
+const fs = require("fs");
+var Autolinker = require("autolinker");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 let filename = process.argv[2];
 if (filename == null) {
-  filename = 'profile-example'
+  filename = "profile-example";
 }
 
-const profile = yaml.safeLoad(fs.readFileSync(`${filename}.yml`, 'utf8'));
+const profile = yaml.safeLoad(fs.readFileSync(`${filename}.yml`, "utf8"));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'personal')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "personal")));
 
-app.get('/', (req, res) => {
-  res.render('index', { profile, Autolinker });
+app.get("/", (req, res) => {
+  res.render("index", { profile, Autolinker });
 });
 
 app.listen(port, () => {
