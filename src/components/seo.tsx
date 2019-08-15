da49f2515c277d0c825911353195ce1f2b3ecdc2
woosik yoon <woosiks.io@gmail.com>
2019-08-16 02:33:41 +0900

    [components,pages] 코드정리

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,10 +1,3 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import { graphql, useStaticQuery } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,22 +6,9 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 const IndexPage = () => {
-  const data = useStaticQuery(graphql`
-  query TempQuery {
-  allResumesYaml {
-    edges {
-      node {
-        name
-      }
-    }
-  }
-}
-
-`);
   return (
     <Layout>
       <SEO title='Home' />
-      <h1>{data.allResumesYaml.edges[0].node.name}</h1>
     </Layout>
   );
 };
