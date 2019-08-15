4fa0158b2b98c5829969cd9a949ac447b3dcf173
woosik yoon <woosiks.io@gmail.com>
2019-08-16 01:20:05 +0900

    [all] yaml transformer 적용

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/resumes/`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,29 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+  query TempQuery {
+  allResumesYaml {
+    edges {
+      node {
+        name
+      }
+    }
+  }
+}
+
+`)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>{data.allResumesYaml.edges[0].node.name}</h1>
+    </Layout>
+  )
+}
 
 export default IndexPage
