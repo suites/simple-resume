466daacfd7aba6745061c2caa431e4bdeb929d80
woosik yoon <woosiks.io@gmail.com>
2019-08-19 22:48:17 +0900

    [component] - title 구조 변경

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -26,7 +26,7 @@ function SEO({ description, lang, meta, title }: { description: string, lang: st
         lang,
       }}
       title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      titleTemplate={`${site.siteMetadata.title}`}
       meta={[
         {
           name: `description`,
